d7a5a8b77d12a8546cd5588a4c401ad451ca8a47
bobenut <kongxh_1981@aliyun.com>
2021-10-08 11:00:13 +0800

    test

--- service-todo/src/router/task.js
+++ service-todo/src/router/task.js
@@ -1,6 +1,6 @@
 const router = require('../infra/routerWrapper');
 const taskService = require('../domain/task/service/taskService');
-
+//
 router.get('/tasks', ctx => {
     ctx.body = taskService.getTasks();
 });
